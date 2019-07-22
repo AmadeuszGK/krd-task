5006a130ab1306809a2df294e881ccc37ffaba67
Amadeus <adek-g@go2.pl>
2019-07-22 18:40:07 +0200

    added more and less button

--- src/DebtsListItem/DebtsListItem.js
+++ src/DebtsListItem/DebtsListItem.js
@@ -1,33 +1,65 @@
-import React from 'react'
+import React from "react";
 
-const DebtsListItem = ({ number, name, value, nip, address, price, documentType }) => {
-  return (
-    <li className="debts-list__item">
-      <div className="debts-list-item__name-addres column-1">
-        <p className="debts-list__item__title">DŁUżNIK</p>
-        <p className="debts-list__item__value">{name}</p>
-        <p className="debts-list__item__title">ADRES</p>
-        <p className="debts-list__item__value">{address}</p>
-      </div>
-      <div className="debts-list__item__nip-doctype column-2">
-        <p className="debts-list__item__title">NIP</p>
-        <p className="debts-list__item__value">{nip}</p>
-        <p className="debts-list__item__title">RODZAJ/TYP DOKUMENTU STANOWIĄCY PODSTAWĘ DLA WIERZYTELNOŚCI</p>
-        <p className="debts-list__item__value">{documentType}</p>
-      </div>
-      <div className="debts-list__item__value-price column-3">
-        <p className="debts-list__item__title">KWOTA ZADŁUENIA</p>
-        <p className="debts-list__item__value">{value}</p>
-        <p className="debts-list__item__title">CENA ZADŁUENIA</p>
-        <p className="debts-list-item__value">{price}</p>
-      </div>
-      <div className="debts-list-item__number column-4">
-        <button className="debts-list__item__button">WIĘCEJ</button>
-        <p className="debts-list__item__title">NUMER</p>
-        <p className="debts-list__item__value">{number}</p>
-      </div>
-    </li>
-  );
-};
+export default class DebtsListItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      itemClass: "debts-list__item"
+    };
+  }
 
-export default DebtsListItem;
+  showMore() {
+    this.setState({
+      itemClass: "debts-list__item debts-list__item--focus"
+    });
+  }
+
+  showLess() {
+    this.setState({
+      itemClass: "debts-list__item"
+    });
+  }
+
+  render() {
+    return (
+      <li className={this.state.itemClass}>
+        <div className="debts-list-item__name-addres column-1">
+          <p className="debts-list__item__title">DŁUżNIK</p>
+          <p className="debts-list__item__value">{this.props.name}</p>
+          <p className="debts-list__item__title">ADRES</p>
+          <p className="debts-list__item__value">{this.props.address}</p>
+        </div>
+        <div className="debts-list__item__nip-doctype column-2">
+          <p className="debts-list__item__title">NIP</p>
+          <p className="debts-list__item__value">{this.props.nip}</p>
+          <p className="debts-list__item__title">
+            RODZAJ/TYP DOKUMENTU STANOWIĄCY PODSTAWĘ DLA WIERZYTELNOŚCI
+          </p>
+          <p className="debts-list__item__value">{this.props.documentType}</p>
+        </div>
+        <div className="debts-list__item__value-price column-3">
+          <p className="debts-list__item__title">KWOTA ZADŁUENIA</p>
+          <p className="debts-list__item__value">{this.props.value}</p>
+          <p className="debts-list__item__title">CENA ZADŁUENIA</p>
+          <p className="debts-list-item__value">{this.props.price}</p>
+        </div>
+        <div className="debts-list-item__number column-4">
+          <button
+            className="debts-list__item__button--more"
+            onClick={this.showMore.bind(this)}
+          >
+            WIĘCEJ
+          </button>
+          <button
+            className="debts-list__item__button--less"
+            onClick={this.showLess.bind(this)}
+          >
+            MNIEJ
+          </button>
+          <p className="debts-list__item__title">NUMER</p>
+          <p className="debts-list__item__value">{this.props.number}</p>
+        </div>
+      </li>
+    );
+  }
+}
