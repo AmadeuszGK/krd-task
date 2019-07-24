037cec6cd70612fd0f2edb30ecc1cf8bf6f0693a
Amadeus <adek-g@go2.pl>
2019-07-24 18:53:02 +0200

    working filter auto update and loading visualisation

--- src/App.js
+++ src/App.js
@@ -1,82 +1,102 @@
-import React from 'react';
-import './App.scss';
-import DebtsList from './DebtsList/DebtsList'
-import Counter from './Counter/Counter'
-import Filter from './Filter/Filter'
+import React from "react";
+import "./App.scss";
+import DebtsList from "./DebtsList/DebtsList";
+import Counter from "./Counter/Counter";
+import Filter from "./Filter/Filter";
+import spinner from "./spinner.gif"
 
-
-export class App extends React.Component {
-  state = {
-    debtsList: [],
-    debtsAmount: '',
-    filterdList: null,
-    loading: false,
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      debtsList: [],
+      debtsAmount: "",
+      loading: false,
+      inputValue: ""
+    };
   }
 
- 
-
-  onFilterList = (filterdList) => {
+  onInputValue = inputValue => {
     this.setState({
-      debtsList: filterdList
-    })
-  }
+      inputValue: inputValue
+    });
+  };
 
-  onLoading = (loading) => {
-    this.setState({
-      loading: loading
-    })
-  }
+  filterList = () => {
+    this.setState({ loading: true });
 
+    fetch(
+      "http://rekrutacja-webhosting.it.krd.pl/api/Recruitment/GetFilteredDebts",
+      {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(this.state.inputValue)
+      }
+    )
+      .then(res => res.json())
+      .then(json => this.setState({ debtsList: json, loading: false }))
+      .catch(error => alert("Wpisz conajmniej 3 znaki!"));
+  };
 
   componentDidMount() {
     fetch("http://rekrutacja-webhosting.it.krd.pl/api/Recruitment/GetTopDebts")
       .then(res => res.json())
       .then(json => this.setState({ debtsList: json }));
 
-    fetch("http://rekrutacja-webhosting.it.krd.pl/api/Recruitment/GetDebtsCount")
+    fetch(
+      "http://rekrutacja-webhosting.it.krd.pl/api/Recruitment/GetDebtsCount"
+    )
       .then(res => res.json())
       .then(json => this.setState({ debtsAmount: json }));
-
-      setTimeout(() => {
-        console.log('debtsList', this.state.debtsList)
-      }, 3000);
-
-      setTimeout(() => {
-        console.log('debtsList', this.state.debtsList)
-      }, 10000);
   }
 
   render() {
-    
+    return (
+      <main>
 
+        <header>
+          <div className="container">
+            <div className="col-12 row header-container">
+              <div className="filter">
+                <p className="filter__title">
+                  Podaj numer sprawy, nazwę lub NIP dłużnika:
+                </p>
+                <Filter
+                  inputValue={this.onInputValue}
+                  filterList={this.onFilterList}
+                  passedFunction={this.filterList}
+                />
+                <p className="filter__tip">*wpisz conajmniej 3 znaki</p>
+              </div>
+              <Counter debtsAmount={this.state.debtsAmount} />
+            </div>
+          </div>
+        </header>
+        
+        <section>
+          <div className="container">
+            <div
+              className="col-12 row debts-list"
+              style={!this.state.loading ? {} : { display: "none" }}>
+              <div className="debts-list__titles">
+                <p className="column-1">dłużnik</p>
+                <p className="column-2">NIP</p>
+                <p className="column-3">kwota zadłużenia</p>
+              </div>
+              <DebtsList debtsList={this.state.debtsList} />
+            </div>
+            <div
+              className="loader"
+              style={this.state.loading ? {} : { display: "none" }}>
+              <img src={spinner} className="loader__image" />
+            </div>
+          </div>
+        </section>
 
-    return(<main>
-      <header>
-        <div className="container">
-          <div className="col-12 row header-container">
-            <div className="filter">
-              <p className="filter__title">Podaj numer sprawy, nazwę lub NIP dłużnika:</p>
-              <Filter filterList={this.onFilterList} loading={this.onLoading}/>
-              <p className="filter__tip">*wpisz conajmniej 3 znaki</p>
-            </div>
-            <Counter debtsAmount={this.state.debtsAmount}/>
-          </div>
-        </div>
-      </header>
-      <section>
-        <div className="container">
-          <div className="col-12 row debts-list">
-            <div className="debts-list__titles">
-              <p className="column-1">dłużnik</p>
-              <p className="column-2">NIP</p>
-              <p className="column-3">kwota zadłużenia</p>
-            </div>
-            <DebtsList debtsList={this.state.debtsList}/>
-          </div>
-        </div>
-      </section>
-    </main>)
+      </main>
+    );
   }
 }
 
-export default App;
--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -1,54 +1,38 @@
-import React from 'react'
+import React from "react";
 
 export class Filter extends React.Component {
-    state = {
-        inputValue: "",
-        filteredList: [],
-        loading: false
-    }
-
-
-
-  handleChange = (event) => {
-      this.setState({ inputValue: event.target.value })
-      console.log('inputValue', this.state.inputValue)
+  constructor() {
+    super();
+    this.state = {
+      inputValue: "",
+      filteredList: [],
+      loading: false
+    };
   }
 
-  filterList = () => {
-    this.setState(prevState => ({ loading: !prevState.loading }))
-    this.props.filterList(this.state.filteredList)
-    this.props.loading(this.state.loading)
-    console.log("loading", this.state.loading)
+  handleChange = event => {
+    this.setState({ inputValue: event.target.value });
+    this.props.inputValue(this.state.inputValue);
+  };
 
-    fetch("http://rekrutacja-webhosting.it.krd.pl/api/Recruitment/GetFilteredDebts", {
-        method: "post",
-        headers: {
-            'Content-Type': 'application/json'
-        }, 
-        body: JSON.stringify(this.state.inputValue)
-    })
-    .then(res => res.json())
-    .then(json => this.setState({ filteredList: json }))
-    // .then(this.setState({ loading: false }))
-    .catch(error => alert("Wpisz conajmniej 3 znaki!"));
-
-    setTimeout(() => {
-        console.log('filter', this.state.filteredList)
-      }, 1000);
-      setTimeout(() => {
-        console.log('filter', this.state.filteredList)
-      }, 4000);
-    this.props.filterList(this.state.filteredList)
+  render() {
+    return (
+      <div className="filter__container">
+        <input
+          className="filter__container__input"
+          placeholder="Imię, nazwisko, NIP, numer..."
+          value={this.state.inputValue}
+          onChange={this.handleChange}
+        />
+        <button
+          className="filter__container__button"
+          onClick={this.props.passedFunction}
+        >
+          SZUKAJ
+        </button>
+      </div>
+    );
   }
-
-    render() {
-        return (
-            <div className="filter__container">
-                <input className="filter__container__input" placeholder="Imię, nazwisko, NIP, numer..." value={this.state.inputValue} onChange={ this.handleChange }></input>
-                <button className="filter__container__button" onClick={ this.filterList } >SZUKAJ</button>
-            </div>
-        );
-    }
-};
+}
 
 export default Filter;
